7e892fe9b291dddf1eabcd0671e8b61b031a6517
Nobuki Fujii <45781612+noi@users.noreply.github.com>
2020-09-12 00:42:28 +0900

    feat: add lightsail dns authenticator
    Add authenticator for the Amazon Lightsail DNS:
    - add authenticator `dns-lightsail`
    - add argument `--dns-lightsail-propagation-seconds` (default: 65)

--- setup.py
+++ setup.py
@@ -1,7 +1,22 @@
 import setuptools
 
+install_requires = [
+    'certbot>=1.8.0',
+    'boto3>=1.14.59',
+    'zope.interface>=5.1.0',
+]
+
+entry_points = {
+    'certbot.plugins': [
+        'dns-lightsail = certbot_dns_lightsail.authentication:Authenticator'
+    ]
+}
+
 setuptools.setup(
     name='certbot-dns-lightsail',
     version='0.0.0',
-    packages=setuptools.find_packages()
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages('src'),
+    install_requires=install_requires,
+    entry_points=entry_points,
 )
---
+++ src/certbot_dns_lightsail/authentication.py
@@ -0,0 +1,77 @@
+import logging
+
+import boto3
+
+from certbot.errors import PluginError
+from certbot.interfaces import IAuthenticator
+from certbot.interfaces import IPluginFactory
+from certbot.plugins.dns_common import DNSAuthenticator
+
+import zope.interface
+
+logger = logging.getLogger(__name__)
+
+
+@zope.interface.implementer(IAuthenticator)
+@zope.interface.provider(IPluginFactory)
+class Authenticator(DNSAuthenticator):
+    description = 'This plugin proves you have control over a domain ' \
+        'by DNS-01 challenge to the Amazon Lightsail DNS.'
+
+    def __init__(self, config, name):
+        super(Authenticator, self).__init__(config, name)
+        self._client = _LightsailClient()
+
+    @classmethod
+    def add_parser_arguments(cls, add):
+        super(Authenticator, cls).add_parser_arguments(
+            add, default_propagation_seconds=65)
+
+    def more_info(self):
+        return self.description
+
+    def _perform(self, domain, validation_domain_name, validation):
+        try:
+            self._client.create_txt_record(
+                domain, validation_domain_name, validation)
+        except Exception as e:
+            raise PluginError(
+                'Failed to create TXT record ({}): {}'.format(
+                    validation_domain_name, e))
+
+    def _cleanup(self, domain, validation_domain_name, validation):
+        try:
+            self._client.delete_txt_record(
+                domain, validation_domain_name, validation)
+        except Exception as e:
+            logger.error(
+                'Failed to delete TXT record ({}): {}'.format(
+                    validation_domain_name, e))
+
+    def _setup_credentials(self):
+        pass
+
+
+class _LightsailClient:
+    def __init__(self):
+        self._client = boto3.client('lightsail')
+
+    def create_txt_record(self, domain, name, value):
+        self._client.create_domain_entry(
+            domainName=domain,
+            domainEntry={
+                'type': 'TXT',
+                'name': name,
+                'target': '"{}"'.format(value)
+            }
+        )
+
+    def delete_txt_record(self, domain, name, value):
+        self._client.delete_domain_entry(
+            domainName=domain,
+            domainEntry={
+                'type': 'TXT',
+                'name': name,
+                'target': '"{}"'.format(value)
+            }
+        )
