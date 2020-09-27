2a922d39107091650bd9341263e6018ef88dbaa9
Nobuki Fujii <45781612+noi@users.noreply.github.com>
2020-09-27 19:24:21 +0900

    chore(build): add extra requirements for development
    Add extras named `dev`.

--- setup.py
+++ setup.py
@@ -6,6 +6,24 @@ install_requires = [
     'zope.interface>=5.1.0',
 ]
 
+dev_extras = [
+    'tox',
+    'flake8',
+    'flake8-import-order',
+    'wheel',
+    'twine',
+]
+
+dev_py27_extras = [
+    'mock',
+    'parsedatetime==2.5',
+]
+
+extras_require = {
+    'dev': dev_extras,
+    'dev:python_version=="2.7"': dev_py27_extras,
+}
+
 entry_points = {
     'certbot.plugins': [
         'dns-lightsail = certbot_dns_lightsail.authentication:Authenticator'
@@ -18,5 +36,6 @@ setuptools.setup(
     package_dir={'': 'src'},
     packages=setuptools.find_packages('src'),
     install_requires=install_requires,
+    extras_require=extras_require,
     entry_points=entry_points,
 )
